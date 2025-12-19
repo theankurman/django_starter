0931912c48794dfc8cc9c20a118e3dfd32b4525a
Ankur Paudel <paudelankur@gmail.com>
2025-12-19 08:10:53 +0000

    feat: setup tailwind and daisyui

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from "vite"
 import { resolve } from "path"
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
     base: "/static/vite/",
+    plugins: [
+        // 
+        tailwindcss(),
+    ],
     build: {
         manifest: "manifest.json",
         outDir: resolve("./core/static/vite"),
         rollupOptions: {
             input: [
-                "core/resources/js/main.ts"
+                // 
+                "core/resources/js/main.ts",
             ]
         }
     }
