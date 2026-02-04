8f91e0bd8439672b6fc4d49268b7b138d0bf76ab
Ankur Paudel <paudelankur@gmail.com>
2026-02-04 09:22:18 +0000

    feat: add workaround for huey for windows

--- manage.py
+++ manage.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
+import multiprocessing
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     try:
         from django.core.management import execute_from_command_line
+
+        if sys.platform == "win32":
+            # huey tries to set multiprocessing start method to "fork" on python 3.14
+            # this doesnt work on windows, below is a workaround
+            multiprocessing.set_start_method(None)
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -18,5 +25,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
